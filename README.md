5b0c2ee42eb96932d7f2ec12144d763549df8246
Praveen Singh <praveensingh.199@gmail.com>
2024-09-25 16:31:35 +0530

    1.3 - New state change with previous state

--- README.md
+++ README.md
@@ -109,3 +109,33 @@ function App() {
 export default App
 
 
+1.3 - New state change with previous state
+-----------------------------------------------------------------------------------------------------------
+App.jsx 
+----------------------------------------------------------------
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+  }
+
+  return (
+    <>
+      <h1>Count : {count}</h1>
+      <button onClick={increaseCount}>Increase by 4</button>
+    </>
+  )
+}
+
+export default App
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  /* const [brand, setBrand] = useState('Ferrari');
-  const [model, setModel] = useState('Roma');
-  const [year, setYear] = useState('2023');
-  const [color, setColor] = useState('red'); */
+  const [count, setCount] = useState(0);
 
-  const [car, setCar] = useState({
-    brand: "Ferrari",
-    model: "Roma",
-    year: "2023",
-    color: "red"
-  });
-
-  const changeColor = () => {
-    setCar((prev) => {
-      return { ...prev, color: "blue" } //here we are getting previous data using ...prev
-    })
+  const increaseCount = () => {
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+    setCount(count => count + 1);
   }
 
   return (
     <>
-      <h1>My {car.brand}</h1>
-      <h2>It us a {car.color} {car.model} form {car.year}</h2>
-      <button onClick={changeColor}>Blue</button>
+      <h1>Count : {count}</h1>
+      <button onClick={increaseCount}>Increase by 4</button>
     </>
   )
 }
