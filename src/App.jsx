d628e3acdd655404477a7a22bd029af2cc6fcbe2
Praveen Singh <praveensingh.199@gmail.com>
2024-09-25 17:19:34 +0530

    2.useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [name, setName] = useState("Greet Stack")
+
+  //This will run on every render because we are not passing 2nd dependency
+  /* useEffect(() => {
+    setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 2000);
+  }); */ 
+
+  //We are using emply array that's why it is loading 1 time
+  /* useEffect(() => {
+    setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 2000);
+  }, []); */ 
+
+  //We are passing count variable in 2nd argument it means whenever count change it will load useEffect
+  /* useEffect(() => {
+    setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 2000);
+  }, [count]); */
+
+  //We can pass multiple state into array means when name changes useEffect will also get loaded
+  useEffect(() => {
+    setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 2000);
+  }, [count, name]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>I have rendered {count} times!</h1>
     </>
   )
 }
