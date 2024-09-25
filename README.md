6b257871cc53b872adf5ea8201848d9359585fc4
Praveen Singh <praveensingh.199@gmail.com>
2024-09-25 19:02:40 +0530

    3.3 - Accessing dom element

--- README.md
+++ README.md
@@ -308,3 +308,30 @@ function App() {
 
 export default App
 
+3.3 - Accessing dom element
+-----------------------------------------------------------------------------------------------------------
+App.jsx 
+----------------------------------------------------------------
+import { useEffect, useState, useRef } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const inputElement = useRef();
+
+  const btnClicked = () => {
+    inputElement.current.style.background = "blue";
+  }
+
+  return (
+    <>
+      <input type="text" ref={inputElement}></input>
+      <button onClick={btnClicked}>Click Here</button>
+    </>
+  )
+}
+
+export default App
+
--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [value, setValue] = useState(0);
 
-  const count = useRef(0);//We have used useRef & store into count 
+  const inputElement = useRef();
 
-  const decreaseCount = () => {
-    setValue(prev => prev - 1)
+  const btnClicked = () => {
+    inputElement.current.style.background = "blue";
   }
-
-  const increaseCount = () => {
-    setValue(prev => prev + 1)
-  }
-
-  /* const effect = () => {
-    setCount(prev => prev + 1)
-  } */
-
-  useEffect(() => {
-    count.current = count.current + 1;
-  })
 
   return (
     <>
-      <button onClick={decreaseCount}>-1</button>
-      <h1>{value}</h1>
-      <button onClick={increaseCount}>+1</button>
-      <h1>Render count : {count.current}</h1>
+      <input type="text" ref={inputElement}></input>
+      <button onClick={btnClicked}>Click Here</button>
     </>
   )
 }
